dffb804cb8c5bf19e612452b07ead82c3375ca2a
Alexander Malekan <alexmalekan@gmail.com>
2017-06-29 20:43:55 -0400

    not found middleware

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const server = express();
 const port = process.env.PORT || 8080;
 
 //middleware imports
-const logger = require('./middleware/logger')
+const logger = require('./middleware/logger');
+const notFound = require('./middleware/404');
 //routers
 const weatherRouter = require('./routers/weather.router');
 //middlware use
 server.use(logger);
 server.use(weatherRouter);
+server.use(notFound);
 // dummy route for testing
 server.get('/', (request, response) => {
   response.send('it works!');
